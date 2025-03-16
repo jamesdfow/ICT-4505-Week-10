2ba6579dd1afca7078283211fd6f2617e8c82aac
James <160443991+jamesdfow@users.noreply.github.com>
2025-03-15 20:52:43 -0600

    added hamburger navbar for mobile

---
+++ Background Page/backgroundPage.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.querySelector(".myLinks");
+    menu.classList.toggle("show");
+  }
+
+  function toggleDetails(jobBox) {
+    jobBox.classList.toggle("active");
+  }
